47da223e3d3d9727108900eb455aa8ed5b9b5dae
Rafal <araszkiewiczrafal@gmail.com>
2015-11-10 20:54:19 +0100

    #0.0.0 First commit

---
+++ Crypto1_DecryptSecretMessage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto1_DecryptSecretMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class DecryptMessage
+    {
+        static public byte[] ConvertStringHexToByteArray(string hexString)
+        {
+            byte[] hexArray = new byte[hexString.Length/2];
+            for (int i = 0, x = 0; i < hexString.Length; i+=2, x++)
+            {
+                // ISSUE: what if length is odd? Can it be odd?
+                hexArray[x] = byte.Parse(hexString.Substring(i, 2));
+            }
+
+            return hexArray;
+        }
+    }
+}
