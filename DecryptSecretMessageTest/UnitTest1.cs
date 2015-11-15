c6f6bcfdeec1cb8f5491352180908b5a0f8390f3
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-11-15 03:54:11 +0100

    #0.0.2 Two parts of decode function complited.

---
+++ DecryptSecretMessageTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Crypto1_DecryptSecretMessage;
+using System.Diagnostics;
+
+namespace DecryptSecretMessageTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FactorialPositiveInputTest()
+        {
+            long[] factorials = new long[]
+            {
+                1,
+                1,
+                2,
+                6,
+                24,
+                120,
+                720,
+                5040,
+                40320,
+                362880,
+                3628800,
+                39916800,
+                479001600,
+                1932053504,
+            };
+
+            int n = 0;
+            foreach (int fac in factorials)
+            {
+                long factorial = DecryptMessage.Factorial(n++);
+                Assert.AreEqual(fac, factorial,"{n}");
+            }
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void FactorialNegativeInputTest()
+        {
+            int n = -1;
+
+            DecryptMessage.Factorial(n);
+        }
+    }
+}
